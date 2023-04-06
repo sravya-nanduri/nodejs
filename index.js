3889ff990f9fbc1ca21e432647a33bb2b11b4959
Ubuntu <ubuntu@ip-172-31-94-147.ec2.internal>
2023-04-06 12:50:59 +0000

    changed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000
 
 app.get("/", (req, res) => {
-    res.send("hello docker")
+    res.send("hello docker!And we the Goal diggers")
 })
 app.listen(port, () => {
     console.log("server running");
