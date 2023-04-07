dcfdf2b97c1bc0025b69fcdf7be9e610b58b7bb6
sravya-nanduri <84439938+sravya-nanduri@users.noreply.github.com>
2023-04-07 14:06:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express');
+pconst express = require('express');
 const app = express();
 
 const port = process.env.PORT || 5000
 
 app.get("/", (req, res) => {
-    res.send("hello docker!we are the Goal diggers")
+    res.send("hello people!we are the Goal diggers")
 })
 app.listen(port, () => {
     console.log("server running");
